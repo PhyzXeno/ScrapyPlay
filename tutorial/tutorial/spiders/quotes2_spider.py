cd67494303839e29bdd0b61810d1e388af37bc2a
PhyzXeno <630172407@qq.com>
2017-08-27 13:39:49 +0800

    tutorial finished
    https://doc.scrapy.org/en/latest/intro/tutorial.html
    
    need to know something about xpath and css by the links referenced in that page

---
+++ tutorial/tutorial/spiders/quotes2_spider.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class Quotes2Spider(scrapy.Spider):
+    name = "quotes2"
+    start_urls = [
+        'http://quotes.toscrape.com/page/3/',
+        'http://quotes.toscrape.com/page/4/'
+    ]
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = "quotes-%s.html" % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
